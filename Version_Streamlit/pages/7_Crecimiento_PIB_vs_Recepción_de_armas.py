acc07355c9ce4c1af98d50c4394e55f26414cd74
Judsuarezag <juan.dsa99@gmail.com>
2026-02-25 19:10:12 -0500

    .

--- Version_Streamlit/pages/7_Crecimiento_PIB_vs_Recepción_de_armas.py
+++ Version_Streamlit/pages/7_Crecimiento_PIB_vs_Recepción_de_armas.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import glob
 import os
-from data import datos_armas, datos_pib, graf_suppliers, graf_recipients, graf_mayor_supplier, graf_mayor_recipient, graf_arma, graf_pib, graf_arms_gdp, graf_rece_gd
+from data import datos_armas, datos_pib, graf_suppliers, graf_recipients, graf_mayor_supplier, graf_mayor_recipient, graf_arma, graf_pib, graf_arms_gdp, graf_rece_gdp
 
 path = '../Datos'
 path2 = '../PIB'
