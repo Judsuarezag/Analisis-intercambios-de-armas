f9bdbb69d95cc936dafb8417cfc7ad0bb7bb1b44
Judsuarezag <juan.dsa99@gmail.com>
2026-03-03 21:18:54 -0500

    z

--- Version_Streamlit/Inicio.py
+++ Version_Streamlit/Inicio.py
@@ -12,6 +12,7 @@ st.title("Análisis de bases de datos sobre Intercambios de Armas", text_alignme
 st.header("Introducción")
 st.text("Este proyecto analiza las bases de datos de la ONU sobre los intercambios de armas a lo largo del tiempo, determinando los principales países involucrados, los tipos de armas más comunes, y la relación entre el crecimiento económico (PIB) y la distribución de armas. A través de visualizaciones interactivas, se busca comprender mejor las dinámicas globales de comercio de armas y su impacto en la economía mundial.")
 st.image("armas3.jpg", width="stretch")
+st.text("Imagen generada por IA", text_alignment="center")
 st.header("Proyecto desarrollado por:")
 st.text("Juan Diego Suárez Agualimpia")
 st.text("Daniel Hurtado")
